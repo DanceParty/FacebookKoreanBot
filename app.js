035c9ae73189a62a9d27cac94b629a3159fda57f
Keevan Dance <jkd65@nau.edu>
2017-05-07 14:41:31 -0700

    fixed env variable

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function sendTextMessage(sender, text) {
     }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: {access_token:PAGE_ACCESS_TOKEN},
         method: 'POST',
         json: {
             recipient: {id:sender},
