1bdc89b250321bd0e4a86ac18e766822329a45e0
Keevan Dance <jkd65@nau.edu>
2017-05-07 15:27:28 -0700

    removed hangul-romanization

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var request = require('request')
 var app = express()
 
 var unirest = require('unirest')
-var hangulRominazation = require('hangul-romanization')
+//var hangulRominazation = require('hangul-romanization')
 
 var PAGE_ACCESS_TOKEN = 'EAAMM1gYOdZBMBAA379aWUrrcy49Q3yrcQ5pVJWtI9LscOMGDGsbiqNZAqZAFiuhsKQ5qVQoVIkvqYB1vZAqTuXvCuHdmgo7ygskf0rKbATWqLBDa7At5ZCM5vNoIqZBvruiZCqP7j2uJfDnsRdamL1g5UEsI5ZCCfSnAmcvJZBPX9PwZDZD';
 
